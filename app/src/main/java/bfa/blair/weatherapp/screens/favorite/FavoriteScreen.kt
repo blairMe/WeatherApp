0e4bd992b40187867d40fff5be7135efd5761009
blairMe <felixanachoni@gmail.com>
2023-01-10 16:59:53 +0300

    Weather App

--- app/src/main/java/bfa/blair/weatherapp/screens/favorite/FavoriteScreen.kt
+++ app/src/main/java/bfa/blair/weatherapp/screens/favorite/FavoriteScreen.kt
@@ -1,10 +1,79 @@
 package bfa.blair.weatherapp.screens.favorite
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import bfa.blair.weatherapp.model.room.Favorite
+import bfa.blair.weatherapp.widgets.WeatherAppBar
 
 @Composable
-fun FavoriteScreen(navController: NavController) {
-    Text(text = "Settings")
+fun FavoriteScreen(navController: NavController,
+                    favoriteViewmodel: FavoriteViewmodel = hiltViewModel()) {
+    Scaffold(
+        topBar = {
+            WeatherAppBar(
+                title = "Favorite Cities",
+                icon = Icons.Default.ArrowBack,
+                isMainScreen = false,
+                navController = navController) {
+                navController.popBackStack()
+            }}) {
+        Surface(modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                val list = favoriteViewmodel.favlist.collectAsState().value
+
+                LazyColumn {
+                    items(items = list) {
+                        CityRow(it, navController = navController, favoriteViewmodel)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CityRow(
+    favorite: Favorite,
+    navController: NavController,
+    favoriteViewmodel: FavoriteViewmodel) {
+    
+    Surface(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(30.dp)
+        .clickable { },
+        shape = CircleShape.copy(topEnd = CornerSize(6.dp)),
+        color = Color(0xFFB2DFDB)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+
+        }
+
+    }
+
 }
